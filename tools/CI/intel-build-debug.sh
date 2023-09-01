d3896e5ac4fd6db62b0e572f8db240fbe307170a
Ivan Scivetti <ivan.scivetti@stfc.ac.uk>
2023-09-01 15:01:33 +0100

    Issue 4: adding Jenkins file

--- tools/CI/intel-build-debug.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source /opt/intel/oneapi/setvars.sh > /dev/null
-
-folder="build-intel-debug"
-rm -rf $folder && mkdir $folder && pushd $folder
-FC=ifort cmake ../  -DCMAKE_BUILD_TYPE=Debug  -DWITH_TESTING=Off
-make
